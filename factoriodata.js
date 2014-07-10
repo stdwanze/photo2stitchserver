0766149c30a5ceb734e3957d8aa4c14c22d6c2f4
stdwanze <stdwanze@gmail.com>
2014-07-10 09:28:29 +0200

    2

--- factoriodata.js
+++ factoriodata.js
@@ -14,7 +14,7 @@ var factorioDataService = factorioDataService || {}; ( function(factorioDataServ
 						max : 0,
 						min : 999999999,
 						sold : 0,
-						date: null
+					//	date: null
 					},
 					
 					keyf: function(doc) {
